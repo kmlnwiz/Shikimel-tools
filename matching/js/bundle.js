1e31ef67de1c4cd3762c3b8ef860cfc21c22d797
kotachiwiz <48175908+kotachiwiz@users.noreply.github.com>
2024-04-19 20:30:23 +0900

    Update bundle.js

--- matching/js/bundle.js
+++ matching/js/bundle.js
@@ -54,7 +54,7 @@ function processFileData(dataList) {
 
         html += `<div class="col px-1 mb-5">
             <div class="card touch-none matching-card card-top-${dataList.data[i][0]} border-primary" style="height:42vh;" data-pair="${dataList.data[i][0].replace('pair', '')}">
-                <div class="card-header fs-1 fw-bold text-center">
+                <div class="card-header fs-2 fw-bold text-center">
                     ${String(i + 1).padStart(2, '0')}
                 </div>
                 <div class="card-body my-0 py-0 fs-3 fw-bold vertical-text align-content-center text-center">
@@ -71,7 +71,7 @@ function processFileData(dataList) {
     for (let i = 0; i < dataList.data.length; i++) {
         html += `<div class="col px-1 mt-5">
             <div class="card touch-none matching-card card-bottom-${dataList.data[i][0]} border-danger" style="height:42vh;">
-                <div class="card-header fs-1 fw-bold text-center">
+                <div class="card-header fs-2 fw-bold text-center">
                     ${String(i + 1).padStart(2, '0')}
                 </div>
                 <div class="card-body my-0 py-0 fs-3 fw-bold vertical-text align-content-center text-center">
