cafb579c0167f14d35e9b4f6c3e34cabfa437e8f
kotachiwiz <48175908+kotachiwiz@users.noreply.github.com>
2024-08-26 07:15:34 +0900

    dobon

--- dobon/js/bundle.js
+++ dobon/js/bundle.js
@@ -19,7 +19,7 @@ function dataLoad() {
             const title = lines.shift().split('\t')[1];
             const dataArray = lines.map(line => line.replace("\r", "").split('\t'));
 
-            const dataList = {
+            dataList = {
                 title: title,
                 data: dataArray
             };
@@ -53,8 +53,8 @@ function processFileData(dataList) {
     }, 0);
     console.log(maxLength);
 
-    let colSize = 'col-3 col-md-3 col-xxl-2';
-    if (maxLength > 11) {
+    let colSize = 'col-4';
+    /*if (maxLength > 11) {
         colSize = 'col-12 col-md-12 col-xxl-6';
     } else if (maxLength > 7) {
         colSize = 'col-12 col-md-6 col-xxl-4';
@@ -62,7 +62,7 @@ function processFileData(dataList) {
         colSize = 'col-6 col-md-4 col-xxl-3';
     } else if (maxLength > 1) {
         colSize = 'col-4 col-md-3 col-xxl-2';
-    };
+    };*/
 
     for (let i = 0; i < dataList.data.length; i++) {
 
@@ -83,13 +83,29 @@ function processFileData(dataList) {
             fontSize = 'fs-1';
         };
 
+        function calculateScale(text) {
+            let length = 0;
+            for (let char of text) {
+                length += (char.match(/[ -~]/)) ? 0.5 : 1;
+            }
+            return Math.min(1, 12 / length);
+        };
+
+        function calculateMargin(text) {
+            let length = 0;
+            for (let char of text) {
+                length += (char.match(/[ -~]/)) ? 0.5 : 1;
+            }
+            return Math.max(0, (length - 12) * 0.2);
+        };
+
         const isCorrect = dataList.data[i][2] == '0' ? 'correctOption' : 'incorrectOption';
 
-        html += `<div class="col ${colSize}" style="padding: 0.125rem !important;"><div id="str_a${i}" class="${isCorrect} str-blind blind-2 py-3 m-0 border bg-white text-dark fw-bold text-center touch-none rounded-3 border-secondary" style="font-size:4.00rem;">${dataList.data[i][1]}<br><small class="fw-nomal fs-3 opacity-50 fw-bold position-absolute text-center" style="width:${String(i).length * 2.8}rem; margin-left: -${String(i).length * 1.40}rem !important; margin-top: -7.10rem !important; pointer-events:none;">${i + 1}</small></div>`;
+        html += `<div class="col ${colSize}" style="padding: 0.125rem !important;"><div id="str_a${i}" class="${isCorrect} str-blind blind-2 p-3 m-0 border bg-white text-dark fw-bold text-center touch-none rounded-3 border-secondary" style="font-size:4.00rem;"><div class="text-nowrap touch-none" style="transform: scaleX(${calculateScale(dataList.data[i][1])}); transform-origin: center; pointer-events:none; margin-left: -${String(dataList.data[i][1]).length > 10 ? calculateMargin(dataList.data[i][1]) : 0}rem;">${dataList.data[i][1]}</div><small class="fw-nomal fs-3 opacity-50 fw-bold position-absolute text-center" style="width:${String(i).length * 2.8}rem; margin-left: -${String(i).length * 1.40}rem !important; margin-top: -7.10rem !important; pointer-events:none;">${i + 1}</small></div>`;
 
         const bgColor = dataList.data[i][2] == '0' ? 'bg-success' : 'bg-danger';
 
-        html += `<div id="str_b${i}" class="str-blind blind-3 col py-3 m-0 border ${bgColor} text-white fw-bold text-center touch-none rounded-3 d-none border-secondary" style="font-size:4.00rem;">${dataList.data[i][1]}<br><small class="fw-nomal fs-3 opacity-75 fw-bold position-absolute text-center" style="width:${dataList.data[i][3].length * 2.8}rem; margin-left: -${dataList.data[i][3].length * 1.40}rem !important; margin-top: -1.40rem !important; pointer-events:none;">${dataList.data[i][3] !== "" ? dataList.data[i][3] : "　"}</small><small class="fw-nomal fs-3 opacity-75 fw-bold position-absolute text-center" style="width:3rem; margin-left: -1.45rem !important; margin-top: -7.10rem !important; pointer-events:none;">${i + 1}</small></div></div>`;
+        html += `<div id="str_b${i}" class="str-blind blind-3 col p-3 m-0 border ${bgColor} text-white fw-bold text-center touch-none rounded-3 d-none border-secondary" style="font-size:4.00rem;"><div class="text-nowrap touch-none" style="transform: scaleX(${calculateScale(dataList.data[i][1])}); transform-origin: center; pointer-events:none;">${dataList.data[i][1]}</div><small class="fw-nomal fs-3 opacity-75 fw-bold position-absolute text-center" style="width:${dataList.data[i][3].length * 2.8}rem; margin-left: -${dataList.data[i][3].length * 1.40}rem !important; margin-top: -1.40rem !important; pointer-events:none;">${dataList.data[i][3] !== "" ? dataList.data[i][3] : "　"}</small><small class="fw-nomal fs-3 opacity-75 fw-bold position-absolute text-center" style="width:3rem; margin-left: -1.45rem !important; margin-top: -7.10rem !important; pointer-events:none;">${i + 1}</small></div></div>`;
     };
 
     if ($('#dobonDisplay').prop('checked')) {
@@ -137,9 +153,9 @@ function toggleFullScreen() {
 
 
 
-$(document).on("click", 'div[id^="str_a"], div[id^="str_b"]', function (event) {
-    const isA = event.target.id.startsWith("str_a");
-    const str_no = event.target.id.replace(isA ? "str_a" : "str_b", "");
+$(document).on("click", 'div[id^="str_a"], div[id^="str_b"]', function (e) {
+    const isA = e.target.id.startsWith("str_a");
+    const str_no = e.target.id.replace(isA ? "str_a" : "str_b", "");
     const targetA = `#str_a${str_no}`;
     const targetB = `#str_b${str_no}`;
 
@@ -147,13 +163,26 @@ $(document).on("click", 'div[id^="str_a"], div[id^="str_b"]', function (event) {
     $(targetB).toggleClass('d-none d-block');
 });
 
-$(document).on("click", '.correctOption', function () {
+$(document).on("click", '.correctOption', function (e) {
+    const id = e.target.id.replace("str_a", "");
+    $('.optionText').html(`<a class="mb-2 me-3 btn btn-outline-indigo d-inline-block" onclick="window.open('https://www.google.co.jp/search?q=${encodeURIComponent(dataList.data[id][1])}', '_blank', 'width=800,height=600'); return false;"><i class="bi bi-google" style="font-size:2.50rem;!important"></i></a>${dataList.data[id][1]}`);
+    $('.answerText').html(dataList.data[id][3]);
+
+    var toast = new bootstrap.Toast($('#toast-correct'));
+    toast.show()
     var audio = new Audio('se/Quiz-Correct_Answer02-2.mp3');
     audio.play();
 });
 
-$(document).on("click", '.incorrectOption', function () {
+$(document).on("click", '.incorrectOption', function (e) {
+    const id = e.target.id.replace("str_a", "");
+    $('.optionText').html(`<a class="mb-2 me-3 btn btn-outline-indigo d-inline-block" onclick="window.open('https://www.google.co.jp/search?q=${encodeURIComponent(dataList.data[id][1])}', '_blank', 'width=800,height=600'); return false;"><i class="bi bi-google" style="font-size:2.50rem;!important"></i></a>${dataList.data[id][1]}`);
+    $('.answerText').html(dataList.data[id][3]);
+
+    var toast = new bootstrap.Toast($('#toast-incorrect'));
+    toast.show()
     var audio = new Audio('se/Quiz-Wrong_Buzzer02-3.mp3');
+
     audio.play();
 });
 
